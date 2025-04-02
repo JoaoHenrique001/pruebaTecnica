bea6fe24bb78210b65fc32f6cc5b90aadb471bb8
joaoHenrique001 <joaogamesbrbrazilhotmail9@gmail.com>
2025-04-02 13:48:23 +0200

    chore(assets): adding some more assets like reset, scss and font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint'],
+  
+  css: [
+    '~/assets/css/reset.css',      // Archivo reset.css
+    '~/assets/css/main.scss',      // Archivo SCSS principal
+  ],
 })
