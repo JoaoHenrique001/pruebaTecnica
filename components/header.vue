df275530a6e31c3f7c7b516a1ed291b273c84630
joaoHenrique001 <joaogamesbrbrazilhotmail9@gmail.com>
2025-04-03 14:24:34 +0200

    feat(styles): part of the header is done 30%

--- components/header.vue
+++ components/header.vue
@@ -1,21 +1,22 @@
 <template>
     <header>
         <div class="header-left">
-            <img src="../assets/images/logo_twitch.png" alt="logo" class="logo">
-            <span>MIDUTCH</span>
-            <img src="../assets/icon/more.svg" alt="" class="more">
+            <div class="logo"><img src="../assets/images/logo_twitch.png" alt="logo">
+                <span>MIDUTCH</span
+                    ></div>
+            <img src="../assets/icon/more.svg" class="more" alt="more">
         </div>
 
         <div class="middle-header">
-            <input type="text">
-            <button><img src="" alt=""></button>
+            <input type="text" placeholder="Search">
+            <button><img src="../assets/icon/search.svg" alt="search"></button>
         </div>
 
         <div class="rigth-header">
-            <img src="" alt="">
+            <div><img src="../assets/icon/notification.svg" alt=""> <span class="notification">63</span></div>
             <button>Log In</button>
             <button>Sign Up</button>
-            <img src="" alt="">
+            <img src="../assets/icon/profile.svg" alt="">
         </div>
 
     </header>
